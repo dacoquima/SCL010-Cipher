ef766a0e634e23e0b4234d96bb4a0c8f9dfe43ad
dacoquima <danimalistica@gmail.com>
2019-06-19 16:45:51 -0400

    Example

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,36 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
-  },
-  decode: () => {
-    /* Acá va tu código */
+
+  encode (textarea, offset) {
+
+    let resultadoCifrado = '';
+
+      for (let i=0; i < textarea.length; i++) {
+      
+      let texto = textarea[i].charCodeAt(0);
+      let ASCII = (texto - 65 + offset) % 26 + 65;
+      let textoCifrado = String.fromCharCode(ASCII);
+      
+      resultadoCifrado += textoCifrado;
+      console.log(resultadoCifrado); 
+      console.log(offset);    
   }
-};
+  return resultadoCifrado; 
+},
+
+  decode (textarea2, offset) {
+
+    let resultadoDescifrado = '';
+
+      for (let i=0; i < textarea2.length; i++) {
+      
+      let texto = textarea2[i].charCodeAt(0);
+      let ASCII = (texto - 65 - offset) % 26 + 65;
+      let textoDescifrado = String.fromCharCode(ASCII);
+      
+      resultadoDescifrado += textoDescifrado;
+
+      console.log(resultadoDescifrado); 
+      console.log(offset);
+}
+return resultadoDescifrado; 
+}};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,107 @@
-/*Acá va tu código*/
+
+let cifrar = document.getElementById('btncifrar'); 
+let descifrar = document.getElementById('btndescifrar');
+
+function fcifrar () {
+    let textarea = document.getElementById('textareaHTML').value;
+    let offset = parseInt(document.getElementById('offsetHTML').value);
+    let resultadoCifrado = cipher.encode(textarea, offset);
+    document.getElementById("resultado").innerHTML=resultadoCifrado;
+    console.log(resultadoCifrado);
+console.log(textarea);
+console.log(offset);
+}
+
+function fdescifrar () {
+let textarea2 = document.getElementById('textareaHTML2').value;
+let offset = parseInt(document.getElementById('offsetHTML').value);
+let resultadoDescifrado = cipher.decode(textarea2, offset);
+}
+
+cifrar.addEventListener('click', fcifrar);
+descifrar.addEventListener('click', fdescifrar);
+
+// Páginas 
+
+let inicio = document.getElementById('pinicio');
+inicio.addEventListener('click', mostrarInicio);
+
+function mostrarInicio () {
+document.getElementById("instrucciones").style.display="none";
+document.getElementById("descifratucarta").style.display="none";
+document.getElementById("cuidados").style.display="none";
+document.getElementById("doulas").style.display="none";
+document.getElementById("testimonios").style.display="none";
+
+document.getElementById("inicio").style.display="block";
+
+}
+
+let instrucciones = document.getElementById('pinstrucciones');
+instrucciones.addEventListener('click', mostrarInstrucciones);
+
+function mostrarInstrucciones () {
+document.getElementById("inicio").style.display="none";
+document.getElementById("descifratucarta").style.display="none";
+document.getElementById("cuidados").style.display="none";
+document.getElementById("doulas").style.display="none";
+document.getElementById("testimonios").style.display="none";
+
+document.getElementById("instrucciones").style.display="block";
+
+}
+
+let descifratucarta = document.getElementById('pdescifratucarta');
+descifratucarta.addEventListener('click', mostrarDescifratucarta);
+
+function mostrarDescifratucarta () {
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("instrucciones").style.display="none";
+    document.getElementById("cuidados").style.display="none";
+    document.getElementById("doulas").style.display="none";
+    document.getElementById("testimonios").style.display="none";
+document.getElementById("descifratucarta").style.display="block";
+
+}
+
+let cuidados = document.getElementById('pcuidados');
+cuidados.addEventListener('click', mostrarCuidados);
+
+function mostrarCuidados () {
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("instrucciones").style.display="none";
+    document.getElementById("descifratucarta").style.display="none";
+    document.getElementById("doulas").style.display="none";
+    document.getElementById("testimonios").style.display="none";
+
+document.getElementById("cuidados").style.display="block";
+
+}
+
+let doulas = document.getElementById('pdoulas');
+doulas.addEventListener('click', mostrarDoulas);
+
+function mostrarDoulas () {
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("instrucciones").style.display="none";
+    document.getElementById("descifratucarta").style.display="none";
+    document.getElementById("cuidados").style.display="none";
+    document.getElementById("testimonios").style.display="none";
+
+document.getElementById("doulas").style.display="block";
+
+}
+
+let testimonios = document.getElementById('ptestimonios');
+testimonios.addEventListener('click', mostrarTestimonios);
+
+function mostrarTestimonios () {
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("instrucciones").style.display="none";
+    document.getElementById("descifratucarta").style.display="none";
+    document.getElementById("cuidados").style.display="none";
+    document.getElementById("doulas").style.display="none";
+
+document.getElementById("testimonios").style.display="block";
+
+}
